5f445f67e1f9fe3b85ca714590bc9c5b92c2dee3
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-13 18:20:58 -0300

    Adiciona e valida as props do componente Attr3

--- src/components/form-components/Attr3.jsx
+++ src/components/form-components/Attr3.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Attr3 extends Component {
   render() {
+    const {
+      cardAttr3,
+      onInputChange,
+    } = this.props;
+
     return (
       <div>
         <label htmlFor="attr3">
           Atributo 3
-          <input type="number" id="attr3" data-testid="attr3-input" />
+          <input
+            type="number"
+            id="attr3"
+            data-testid="attr3-input"
+            name="cardAttr3"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
       </div>
     );
@@ -14,3 +27,8 @@ class Attr3 extends Component {
 }
 
 export default Attr3;
+
+Attr3.propTypes = {
+  cardAttr3: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
