90b3644b45827966de398c157be9db9079d0bcc2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-13 18:43:46 -0300

    Adiciona e valida as props do componente SaveButton

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -21,7 +21,7 @@ class Form extends Component {
       cardImage,
       cardRare,
       cardTrunfo,
-      hasTrunfo,
+      // hasTrunfo,
       isSaveButtonDisabled,
       onInputChange,
       onSaveButtonClick,
@@ -63,7 +63,7 @@ class Form extends Component {
         />
         <SaveButton
           onSaveButtonClick={ onSaveButtonClick }
-          onInputChange={ onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
       </form>
     );
@@ -81,7 +81,7 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
--- src/components/form-components/SaveButton.jsx
+++ src/components/form-components/SaveButton.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SaveButton extends Component {
   render() {
+    const {
+      isSaveButtonDisabled,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <div id="save-button">
-        <button type="submit" data-testid="save-button">Salvar</button>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
       </div>
     );
   }
 }
 
 export default SaveButton;
+
+SaveButton.propTypes = {
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
