161a0d859f0474f379a06d5848d2917a0b5af016
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-13 18:36:33 -0300

    Adiciona e valida as props do componente Trunfo

--- src/components/form-components/Trunfo.jsx
+++ src/components/form-components/Trunfo.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Trunfo extends Component {
   render() {
+    const {
+      cardTrunfo,
+      onInputChange,
+    } = this.props;
+
     return (
       <div>
         <label htmlFor="trunfo">
           Trunfo
-          <input type="checkbox" id="trunfo" data-testid="trunfo-input" />
+          <input
+            type="checkbox"
+            id="trunfo"
+            data-testid="trunfo-input"
+            name="cardTrunfo"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
         </label>
       </div>
     );
@@ -14,3 +27,8 @@ class Trunfo extends Component {
 }
 
 export default Trunfo;
+
+Trunfo.propTypes = {
+  cardTrunfo: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
