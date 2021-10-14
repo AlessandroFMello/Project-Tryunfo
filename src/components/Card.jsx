c110ca29d3618d7ba2dcc335eb3a25d3b134e02b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-13 21:55:14 -0300

    Renderiza o componente cardRare na tela

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,6 +26,7 @@ class Card extends Component {
         <h3 data-testid="attr1-card">{ cardAttr1 }</h3>
         <h3 data-testid="attr2-card">{ cardAttr2 }</h3>
         <h3 data-testid="attr3-card">{ cardAttr3 }</h3>
+        <h3 data-testid="rare-card">{ cardRare }</h3>
       </section>
     );
   }
